9c7050971468a88862c6d302775beb710d4f1f13
prmze <99100344+prmze-v2@users.noreply.github.com>
2022-02-16 01:07:05 +0000

    Fixed Vertical Gradient math, also added file writing.

--- main.py
+++ main.py
@@ -2,26 +2,27 @@ import os
 from os import path, sys, system
 from time import sleep
 
+with open('output.tfx', 'w') as f:
+    f.write("")
+
 if sys.argv[1] == "verticle":
-
-    def horizontal_gradient(rgb_code, text):
-        print(rgb_code + text, end='', flush=True)
-
-    with open(sys.argv[2], 'r') as f:
-        startrgb = 0,255,255
-        endrgb = 255,255,0
-        r = int(startrgb[0]) #initial red value
-        g = int(startrgb[1]) #initial green value
-        b = int(startrgb[2]) #initial blue value
-        for x in f.readlines():
-            changer = int((int(endrgb[0]) - int(startrgb[0]))/len(x))
-            changeg = int((int(endrgb[1]) - int(startrgb[1]))/len(x))
-            changeb = int((int(endrgb[2]) - int(startrgb[2]))/len(x))
-            rgb_code = f"\x1b[40;38;2;{r};{g};{b}m"
-            r+=changer  
-            g+=changeg 
-            b+=changeb
-            horizontal_gradient(rgb_code, x)
+    Banner = open(sys.argv[2], "r", encoding="utf8").read()
+    output_lines = Banner.split("\n")
+    startrgb = 0,255,255
+    endrgb = 255,255,0
+    r = int(startrgb[0]) #initial red value
+    g = int(startrgb[1]) #initial green value
+    b = int(startrgb[2]) #initial blue value
+    for x in output_lines:
+        changer = int((int(endrgb[0]) - int(startrgb[0]))/len(output_lines))
+        changeg = int((int(endrgb[1]) - int(startrgb[1]))/len(output_lines))
+        changeb = int((int(endrgb[2]) - int(startrgb[2]))/len(output_lines))
+        rgb_code = f"\x1b[40;38;2;{r};{g};{b}m"
+        r+=changer  
+        g+=changeg 
+        b+=changeb
+        with open('output.tfx', 'a', encoding='utf8') as f:
+            f.write(f"{rgb_code}{x}\n")
 
 
 elif sys.argv[1] == "horizontal":
@@ -37,11 +38,12 @@ elif sys.argv[1] == "horizontal":
 
         for letter in text:
             if letter == '\n':pass
-            print(f"\x1b[40;38;2;{r};{g};{b}m{letter}",end="")
+            with open('output.tfx', 'a', encoding='utf8') as f:
+                f.write(f"\x1b[40;38;2;{r};{g};{b}m{letter}")
             r+=changer 
             g+=changeg 
             b+=changeb
-    with open(sys.argv[2], 'r') as f:
+    with open(sys.argv[2], 'r', encoding='utf8') as f:
         for x in f.readlines():
             startrgb = 0,255,255
             endrgb = 255,255,0
